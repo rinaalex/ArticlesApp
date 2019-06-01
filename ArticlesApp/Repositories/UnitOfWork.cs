a3c81b59178d305409dba007b25b9e894f22c74f
Katherine Burova <katherineburova@gmail.com>
2019-06-01 12:38:40 +0300

    Added UnitOfWork

---
+++ ArticlesApp/Repositories/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ArticlesApp.Interfaces;
+using ArticlesApp.Model;
+
+namespace ArticlesApp.Repositories
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly ArticlesContext context;
+
+        public UnitOfWork(ArticlesContext context)
+        {
+            this.context = context;
+            Authors = new AuthorRepository(context);
+            Articles = new ArticleRepository(context);
+            Reviews = new ReviewRepository(context);
+        }
+
+        public IAuthorRepository Authors { get; private set; }
+        public IArticleRepository Articles { get; private set; }
+        public IReviewRepository Reviews { get; private set; }
+
+        public int Complete()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
