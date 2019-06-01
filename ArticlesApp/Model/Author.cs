4ff7d047180a40ee83acdd73b4317e0103e7e4db
Katherine Burova <katherineburova@gmail.com>
2019-06-01 09:50:42 +0300

    Добавьте файлы проекта.

---
+++ ArticlesApp/Model/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticlesApp.Model
+{
+    public class Author
+    {
+        public int Id { get; set; }
+    }
+}
