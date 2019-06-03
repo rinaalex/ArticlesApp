bb9a47ef19119f38a7f020e9fd9eea319d4e9e44
Katherine Burova <katherineburova@gmail.com>
2019-06-03 21:34:41 +0300

    Added AuthorsController

---
+++ ArticlesApp/Controllers/AuthorsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ArticlesApp.Repositories;
+using ArticlesApp.Model;
+
+namespace ArticlesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly UnitOfWork unitOfWork;
+        public AuthorsController(ArticlesContext context)
+        {
+            unitOfWork = new UnitOfWork(context);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(unitOfWork.Authors.All());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(unitOfWork.Authors.Get(id));
+        }
+    }
+}
