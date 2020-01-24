ffa33df15b8558880aec6e7786bdec76ef57d1e6
Katherine Burova <katherineburova@gmail.com>
2020-01-24 21:17:53 +0300

    Static keyword to Program class added

--- ArticlesApp/Program.cs
+++ ArticlesApp/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ArticlesApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
